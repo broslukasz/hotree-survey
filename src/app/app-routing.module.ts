a8b5ebac4432c50e3f9579201020946bad7e854d
lubros <lukasz.bros.sp@gmail.com>
2019-04-07 12:45:18 +0200

    components created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NewEventComponent } from './new-event/new-event.component';
+import { SummaryComponent } from './summary/summary.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: NewEventComponent},
+  { path: 'summary', component: SummaryComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from '../core/core.module';
+import { NewEventComponent } from './new-event/new-event.component';
+import { SummaryComponent } from './summary/summary.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewEventComponent,
+    SummaryComponent
   ],
   imports: [
     BrowserModule,
