4561e21ba14f03948b8367eee5359c4785d73fc0
lubros <lukasz.bros.sp@gmail.com>
2019-04-07 13:17:03 +0200

    components styles structure

--- src/app/new-event/new-event.component.ts
+++ src/app/new-event/new-event.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-new-event',
   templateUrl: './new-event.component.html',
-  styleUrls: ['./new-event.component.sass']
+  styleUrls: ['./new-event.component.scss']
 })
 export class NewEventComponent {
 
