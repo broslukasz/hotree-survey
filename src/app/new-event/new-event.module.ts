a220c339b9a1ed69e82d413c54c1d844dfca1f35
lubros <lukasz.bros.sp@gmail.com>
2019-04-10 09:44:55 +0200

    formsModule removed

--- src/app/new-event/new-event.module.ts
+++ src/app/new-event/new-event.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { NewEventComponent } from './new-event.component';
 import { HotreeTooltipComponent } from './hotree-tooltip/hotree-tooltip.component';
 import { HotreeFormFieldComponent } from './hotree-form-field/hotree-form-field.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +14,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    FormsModule,
   ]
 })
 export class NewEventModule { }
